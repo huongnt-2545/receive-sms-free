fae5567888c9c38053d53bf8517b3c7f71070f53
huongnt3 <huongnt3@vnext.com.vn>
2021-05-27 13:38:17 +0700

    [dev] List inactive phone numbers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import express, { Request, Response } from 'express';
 const routes = express.Router();
 import phoneNumber from './phoneNumber.route';
 import country from './country.route';
+import inactivePhoneNumber from './inactivePhoneNumber.route';
 
 routes.get('/', (req: Request, res: Response) => {
   res.send('index')
 });
 routes.use('/phone-number', phoneNumber);
 routes.use('/regions', country);
+routes.use('/', inactivePhoneNumber);
 
 export default routes;
