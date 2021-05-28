714ddb67e7360c2cf9ab9d76c5d0dee28087532e
huongnt3 <huongnt3@vnext.com.vn>
2021-05-28 18:00:46 +0700

    Add validate when check country code

--- src/controllers/country.controller.ts
+++ src/controllers/country.controller.ts
@@ -3,32 +3,22 @@ import { Response, Request } from 'express';
 import { getCountry } from '../services/country.service';
 import { getPhoneNumberByCountry } from '../services/phoneNumber.service';
 import logger from '../config/logger';
-import { REGEX_COUNTRY_CODE } from '../utils/constants';
+import { validateCountryCode } from '../validations/country.validation';
 
 export const getPhoneNumbers = async(req: Request, res: Response) => {
   try {
     const countryCode = req.params.code;
-    
-    if (!validateCountryCode(countryCode)) {
-      res.render('404');
-    } else {
-      const country = await getCountry(countryCode);
+    if (!validateCountryCode(countryCode)) res.status(404)
 
-      if (!country) {
-        res.render('404');
-      } else {
-        const phoneNumbers = await getPhoneNumberByCountry(country._id);
-        const result = {country, phoneNumbers}
+    const country = await getCountry(countryCode);
+    if (!country) return res.render('404');
 
-        res.render('countries/show', result);
-      }
-    }
+    const phoneNumbers = await getPhoneNumberByCountry(country._id);
+    const result = {country, phoneNumbers}
+
+    res.render('countries/show', result);
   } catch(err) {
     logger.error(err)
     res.status(500).send(req.t('shared.internal_server_err'));
   }
-}
-
-const validateCountryCode = (code: string) => {
-  return REGEX_COUNTRY_CODE.test(code);
 }
